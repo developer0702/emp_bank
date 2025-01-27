23f19765299536fde6cec2a62cbd275669cce0cc
crajamo <chalamalasetti.rajamounikadevi@optum.com>
2025-01-27 16:53:15 +0530

    Add code changes for group name search

--- src/main/java/com/optum/ftps/ob/core/constants/EmployerDetailsConstants.java
+++ src/main/java/com/optum/ftps/ob/core/constants/EmployerDetailsConstants.java
@@ -5,6 +5,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EmployerDetailsConstants {
-  public static final int EMPLOYER_GROUPNUMBER_LENGTH = 9;
-  public static final int EMPLOYER_GROUPNAME_LENGTH = 40;
+    public static final int EMPLOYER_GROUPNUMBER_LENGTH = 9;
+    public static final int EMPLOYER_GROUPNAME_LENGTH = 40;
 }
--- src/main/java/com/optum/ftps/ob/core/employerDetails/controller/EmployerDetailsController.java
+++ src/main/java/com/optum/ftps/ob/core/employerDetails/controller/EmployerDetailsController.java
@@ -6,10 +6,8 @@ import com.optum.ftps.ob.core.employerDetails.model.v1.EmployerDetailsResponse;
 import com.optum.ftps.ob.core.employerDetails.service.EmployerDetailsService;
 import com.optum.ftps.ob.core.employerDetails.util.StringUtil;
 import com.optum.ftps.ob.core.employerDetails.validator.EmployerDetailsValidator;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,70 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class EmployerDetailsController implements EmployerDetailsApi {
-    private final EmployerDetailsValidator employerDetailsValidator;
-    private final EmployerDetailsService employerDetailsService;
-    private final EmployerDetailsResponseMapper employerDetailsResponseMapper;
+  private final EmployerDetailsValidator employerDetailsValidator;
+  private final EmployerDetailsService employerDetailsService;
+  private final EmployerDetailsResponseMapper employerDetailsResponseMapper;
 
-    @Override
-    public ResponseEntity<EmployerDetailsResponse> getEmployerDetailsById(String empGroupId) {
-        empGroupId = StringUtil.sanitize(empGroupId);
-        log.debug("empId", empGroupId);
-
-        var errors = employerDetailsValidator.validateEmployerDetailsById(empGroupId);
-        // if (!errors.isEmpty()) {
-        // log.debug("Validation failed for employer group id: {}", empGroupId);
-        // exceptionService.handleValidationError(errors);
-        // }
-        var employerDetailsResponseDTO = employerDetailsService.getEmployerDetailsById(empGroupId);
-        var response =
-                employerDetailsResponseMapper.employerDetailsResponse(employerDetailsResponseDTO);
-        /*EmployerDetailsResponse response = new EmployerDetailsResponse();
-        Map<String, Object> responseDetails = new HashMap<>();
-        response.setRequestId("1");
-
-        Status status = new Status();
-        status.setStatusCode("0000");
-        status.setSeverity("INF");
-        status.setStatusDescription("REQUEST SUCCESSFULLY PROCESSED");
-        response.setStatus(status);
-
-        List<Employer> employerList = new ArrayList<>();
-        Employer employer = new Employer();
-        employer.setEmployerGroupId("4H9960");
-        employer.setEmployerName("BMC SOUTHEAST");
-        employer.setEmployerSetupStatus("Active");
-        employer.setUhGLineOfBusiness("Small Business");
-
-        Address address = new Address();
-        AddressTypeCode addressTypeCode = new AddressTypeCode();
-        addressTypeCode.setCode("B");
-        addressTypeCode.setCodeName("Business");
-        address.setAddressTypeCode(addressTypeCode);
-        address.setPrimaryAddressIndicator("Y");
-
-        AddressLines addressLines = new AddressLines();
-        addressLines.setAddressDeliveryLine1("1406 HAYS ST UNIT 8");
-        addressLines.setAddressDeliveryLine2("POSTSTREET");
-        addressLines.setAddressCity("TALLAHASSEE");
-
-        AddressState addressState = new AddressState();
-        addressState.setStateCode("FL");
-        addressState.setStateName("AP");
-        addressLines.setAddressState(addressState);
-
-        AddressPostalCode addressPostalCode = new AddressPostalCode();
-        addressPostalCode.setZipCode("0000");
-        addressPostalCode.setZipPlus4("32301");
-        addressPostalCode.setPostalCode("0000");
-        addressLines.setAddressPostalCode(addressPostalCode);
-
-        address.setAddressLines(addressLines);
-        employer.setAddress(address);
-
-        // Add other fields similarly...
-        employerList.add(employer);
-        response.setEmployer(employerList);*/
-
-        return new ResponseEntity<EmployerDetailsResponse>(response, HttpStatus.OK);
+  @Override
+  public ResponseEntity<EmployerDetailsResponse> getEmployerDetailsById(String employerGroupId) {
+    if (employerGroupId != null) {
+      String empGroupId = StringUtil.sanitize(employerGroupId);
+      log.debug("empId", empGroupId);
+      var errors = employerDetailsValidator.validateEmployerDetailsById(empGroupId);
+      // if (!errors.isEmpty()) {
+      // log.debug("Validation failed for employer group id: {}", empGroupId);
+      // exceptionService.handleValidationError(errors);
+      // }
+      var employerDetailsResponseDTO = employerDetailsService.getEmployerDetails(empGroupId, true);
+      var response =
+          employerDetailsResponseMapper.employerDetailsResponse(employerDetailsResponseDTO);
+      return new ResponseEntity<EmployerDetailsResponse>(response, HttpStatus.OK);
+    } else {
+      log.debug("Employer group id is null");
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Override
+  public ResponseEntity<EmployerDetailsResponse> getEmployerDetailsByName(
+      String employerGroupName) {
+    if (employerGroupName != null) {
+      String empGroupName = StringUtil.sanitize(employerGroupName);
+      log.debug("empName", empGroupName);
+      var errors = employerDetailsValidator.validateEmployerDetailsByName(empGroupName);
+      // if (!errors.isEmpty()) {
+      // log.debug("Validation failed for employer group name: {}", empGroupName);
+      // exceptionService.handleValidationError(errors);
+      // }
+      var employerDetailsResponseDTO =
+          employerDetailsService.getEmployerDetails(empGroupName, false);
+      var response =
+          employerDetailsResponseMapper.employerDetailsResponse(employerDetailsResponseDTO);
+      return new ResponseEntity<EmployerDetailsResponse>(response, HttpStatus.OK);
+    } else {
+      log.debug("Employer group name is null");
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
 }
